fbc6509b052e8f649b1094c533d7436056c43b9c
akshay.innoc <akshay@innocirc.com>
2020-05-21 17:15:27 +0530

    build for github

--- src/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from "redux";
-import products from "./product_reducers";
-
-
-const rootReducers = combineReducers({
-    products
-})
-
-export default rootReducers;
--- src/store.js
+++
@@ -1,6 +0,0 @@
-import { applyMiddleware, createStore } from "redux";
-import promiseMiddleware from "redux-promise";
-
-const store = applyMiddleware(promiseMiddleware)(createStore);
-
-export default store;
