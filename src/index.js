c6ae321eeb2c917981bd8711eb878ba5c2d4f7ab
akshay.innoc <akshay@innocirc.com>
2020-09-26 22:04:10 +0530

    Commit

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import React, { Suspense } from "react";
+import React from "react";
 import ReactDOM from "react-dom";
-import { Router, Route } from "react-router-dom";
+import { Router, Route, Switch } from "react-router-dom";
 import { Provider } from "react-redux";
 import 'bootstrap/scss/bootstrap.scss';
 import 'font-awesome/scss/font-awesome.scss';
@@ -10,7 +10,6 @@ import Cart from "./components/cart";
 import Footer from "./components/footer";
 import rootReducers from "./reducers";
 import store from "./store";
-import Loader from "./components/loader";
 import Header from "./components/header";
 import history from './history';
 
@@ -18,12 +17,11 @@ ReactDOM.render(
     <Provider store={store(rootReducers)}>
         <Header />
         <Router history={history} basename={process.env.PUBLIC_URL}>
-            <Suspense fallback={<Loader />}>
-                <div>
-                    <Route exact path="/" component={App} />
-                    <Route exact path="/cart" component={Cart} />
-                </div>
-            </Suspense>
+            <Switch>
+                <Route exact path="/" component={App} />
+                <Route exact path="/cart" component={Cart} />
+                <Route component={() => (<div>404 Not found</div>)} />
+            </Switch>
         </Router>
         <Footer />
     </Provider>,
